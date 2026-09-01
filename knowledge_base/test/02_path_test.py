44f8b2f69722f908f557ec92e8cc65c06b4d3649
banban <yhy031204@gmail.com>
2026-09-01 20:10:03 +0800

    节点1_node_entry补充及测试

--- knowledge_base/test/02_path_test.py
+++ knowledge_base/test/02_path_test.py
@@ -1,8 +1,19 @@
 """
     测试：Path
 """
+import os
+import sys
 from pathlib import Path
 
 # 获取项目的根路径
-print(Path(__file__).parent.parent)
-print(Path(__file__).parents[1])
+# print(Path(__file__).parent.parent)
+# print(Path(__file__).parents[1])
+
+# 获取文件的标题(不用path对象)
+# print(Path(__file__).stem)
+# print(os.path.basename(__file__).split(".")[0])
+
+def test_fun():
+    # 动态获取当前的函数名
+    print(sys._getframe().f_code.co_name)
+test_fun()
