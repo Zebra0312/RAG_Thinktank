55dff92c778fd9bc84d2a7b3ce24e578383b3cdf
banban <yhy031204@gmail.com>
2026-09-07 19:51:14 +0800

    检索节点1node_item_name_confirm.py中增加保存消息的调用

--- knowledge_base/app/query_process/agent/nodes/node_item_name_confirm.py
+++ knowledge_base/app/query_process/agent/nodes/node_item_name_confirm.py
@@ -1,13 +1,18 @@
+import sys
+
+from app.clients.mongo_history_utils import save_chat_message
 from app.core.logger import node_log
 from app.query_process.agent.state import QueryGraphState
 from app.utils.task_utils import add_running_task, add_done_task
 
 
 @node_log("node_item_name_confirm")
-def node_item_name_confirm(state: QueryGraphState):
-    # 记录当前任务的状态为进行中
-    add_running_task(state["session_id"], "node_item_name_confirm", state["is_stream"])
+def node_item_name_confirm(state):
+    print(f"---node_item_name_confirm 处理")
 
-    # 记录当前任务的状态为已完成
-    add_done_task(state["session_id"], "node_item_name_confirm", state["is_stream"])
-    return state
+    add_running_task(state['session_id'], sys._getframe().f_code.co_name, state.get("is_stream"))
+    add_done_task(state['session_id'], sys._getframe().f_code.co_name, state.get("is_stream"))
+
+    save_chat_message(state['session_id'], "user", state['original_query'], "", state.get("item_names", []))
+
+    print(f"---已保存对话 处理完成")
